1f09dd653a88198a7ccb8675537d3871fe33ed1d
LuManci <lucass.mancinii04@gmail.com>
2023-10-10 01:44:12 -0300

    Prueba

--- SamsungApp/models.py
+++ SamsungApp/models.py
@@ -14,13 +14,13 @@ class Celular(models.Model):
               #('serie Z', 'Serie Z')
               #('serie M', 'Serie M')
               #]
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     nombre = models.CharField(max_length=40)
     serie = models.CharField(max_length=20)
     precio = models.DecimalField(max_digits= 8, decimal_places=2)
     memoria = models.CharField(max_length=20)
     pantalla = models.DecimalField(max_digits=2, decimal_places=1)
     camara = models.DecimalField(max_digits=3, decimal_places=1)
+    publicado = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="publicado")
     fechaPublicacion = models.DateTimeField(auto_now_add=True)
     imagenCelular = models.ImageField(null=True, blank=True, upload_to="media")
     
@@ -28,19 +28,17 @@ class Celular(models.Model):
         return f'{self.id} {self.nombre} {self.serie} {self.precio} {self.memoria}'
 
     class Meta:
-        ordering = ['usuario', '-fechaPublicacion']
+        ordering = ['-fechaPublicacion']
     
     
 class Post(models.Model):
     
-    post =models.ForeignKey(User, on_delete=models.CASCADE, related_name='Post', null=True)
     nombre = models.CharField(max_length=40)
     mensaje = models.TextField(null=True, blank=True)
     fechaComentario = models.DateTimeField(auto_now_add=True)
+    destinatario = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='destinatario')
 
     class Meta:
         ordering = ['-fechaComentario']
 
-    def __str__(self):
-        return '%s - %s' % (self.nombre, self.post)
     
